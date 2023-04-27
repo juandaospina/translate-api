69202c1ac0c4533eeae11653576443e53a7d0ed5
Juan Ospina <juan.ospinal@outlook.com>
2023-04-27 11:48:18 -0500

    fix: cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const { translateRoute } = require("./routes/translate.route");
 const { errorHandler } = require("./middlewares/error.handler");
 
-const enableDomains = ['https://translation-web.vercel.app/']
+const enableDomains = ['https://translation-web.vercel.app']
 
 // Server
 const app = express();
