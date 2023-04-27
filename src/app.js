cf1a2fc21aa43849fce51a4089eecfaf86a0a7c5
Juan Ospina <juan.ospinal@outlook.com>
2023-04-27 11:32:59 -0500

    Add cors config

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
+
+const cors = require('cors');
 require("dotenv").config();
 
 const { translateRoute } = require("./routes/translate.route");
 const { errorHandler } = require("./middlewares/error.handler");
+
+const enableDomains = ['https://translation-web.vercel.app/']
 
 // Server
 const app = express();
 // Route
 app.use(translateRoute);
 // Middlewares
+app.use(cors(enableDomains));
 app.use(errorHandler);
 
 module.exports = app;
