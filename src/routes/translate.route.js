6092e5e9456547c81e34278b98ed7bae56dae067
Juan Ospina <juan.ospinal@outlook.com>
2023-04-27 00:56:35 -0500

    Add translate service

---
+++ src/routes/translate.route.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const { TranslateService } = require("../services/translate.service");
+
+const router = express.Router();
+
+const service = new TranslateService();
+
+router.get("/languages", async (req, res, next) => {
+  try {
+    const response = await service.languages();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log("Algo malo paso");
+    next(error);
+  }
+});
+
+router.post("/translate", async (req, res, next) => {
+  const { q, source, target } = req.query;
+  try {
+    console.log("[/translate]");
+    const response = await service.translate(source, target, (text = q));
+    console.log("[/translate_response_route]", response);
+    res.status(200).json({
+      status: 200,
+      data: {
+        translation: response,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = {
+  translateRoute: router,
+};
---
+++ src/services/translate.service.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+
+const { instance } = require("../libs");
+
+class TranslateService {
+  constructor() {}
+
+  async detectLanguage(text) {
+    const params = `q=${text}`;
+    const result = await instance.post(`/v2/detect?${params}`).then(response => {
+      return response.data.data.detections[0][0].language;
+    })
+    .catch(err => {
+      console.log("[/detect_error]", err.response.status)
+      throw Error(err.response.status);
+    })
+
+    return result;
+  }
+
+  async translate(source, target, text) {
+    let language = source;
+    if (source === "auto") {
+      language = await this.detectLanguage(text);
+    }
+    const res = await instance.post(`/v2?q=${text}&source=${source}&target=${target}`)
+      .then(result => {
+        return result.data.data.translations[0].translatedText;
+      })
+      .catch(err => {
+        console.log("[/translate_error]", err.response.status)
+        throw Error(err.response.status)
+      })
+
+    return res;
+  }
+
+  async languages() {
+    const result = await instance.get('/v2/languages').then(response => {
+      return response.data
+    }).catch(error => {
+      throw Error(error.response.status)
+    })
+
+    return result;
+  }
+}
+
+module.exports = {
+  TranslateService,
+};
